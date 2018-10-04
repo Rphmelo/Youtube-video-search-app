6dfeb3ff21c452548c824825ed64641638dec467
DIR\r.de.melo.silva <r.de.melo.silva@accenture.com>
2018-10-04 16:07:23 -0300

    add video detail component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // Your Youtube API Key goes here
 const YOUTUBE_API_KEY = "AIzaSyDIT1NrVC4RTfOOQsNPn9weCiBbFJD0_QU";
@@ -11,18 +12,25 @@ const YOUTUBE_API_KEY = "AIzaSyDIT1NrVC4RTfOOQsNPn9weCiBbFJD0_QU";
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = { videos: []};
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
         
         YTSearch({key: YOUTUBE_API_KEY, term: 'reactjs'}, (videos) => {
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render(){
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <SearchBar/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
             </div>
         )
     }
